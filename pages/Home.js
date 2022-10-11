60fe771bfcc5ca46df39447cb5482fa58078f254
Patrícia Bandeira <patybandeira.26@gmail.com>
2022-10-10 21:24:24 -0300

    commit
    teste

--- pages/Home.js
+++ pages/Home.js
@@ -4,11 +4,13 @@ import Vector from '../img/Vector.png'
 export default function Home (){
   return (
    <View style={Css.container}>
-   
     <View style={Css.cabecalho}>
     <Image source={Vector} style={Css.img} />
     </View>
+     <View>
 
+      
+     </View>
     </View>
   );
 }
