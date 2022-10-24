60370d91caa4c2a3ea9fcff7064b3598dfd5c757
Patrícia Bandeira <patybandeira.26@gmail.com>
2022-10-23 22:35:22 -0300

    commit
    .

--- pages/Notificacao.js
+++ pages/Notificacao.js
@@ -5,7 +5,7 @@ import nadaPorAqui from '../img/nadpAquiNotifi.png'
 import Container_notificacao from '../Componentes/componente.notificacao';
 
 export default function Notificacao (){
-  let notificacoes = 0;
+  let notificacoes = 1;
   return (
    <View style={Css.container}>
    
--- pages/Perfil relacionados/configucacoes.js
+++ pages/Perfil relacionados/configucacoes.js
@@ -5,16 +5,82 @@ import Checkbox from 'expo-checkbox';
 import Vector from '../../img/Vector.png';
 import voltar from '../../img/voltar.png';
 import { useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 export default function Configuracoes (){
+  const [darkMode, setDarkMode] = useState(false);
+
+  const styles = StyleSheet.create({ 
+    btnIconVoltar:{
+      alignSelf: 'flex-start',
+      top: -10, 
+    },
+    icon:{
+      width: 27,
+      height: 27,
+    },
+
+    ViewConfig:{
+      flex:0.12,
+      backgroundColor: darkMode ? '#000' : 'white',
+      borderWidth:1,
+      borderBottomColor: darkMode ? '#d6d6d6' : 'black',
+    },
+    textCheck:{
+    color: darkMode ? '#d6d6d6' : 'black',
+    textAlign:'left',
+    fontSize:18,
+    marginTop:30,
+    marginLeft:25,
+    fontWeight:'semi bold'
+    },
+    Checkbox:{
+      marginLeft:310,
+      marginTop:-40,
+      backgroundColor:darkMode ? '#25252A' : '#C6C6C6',
+      height:40,
+      width:40,
+      
+    },
+    Mode:{
+width:40,
+height:40,
+backgroundColor: darkMode ? '#25252A' : '#C6C6C6',
+marginLeft:310,
+marginTop:-40,
+
+    },
+    btnConfig:{
+      backgroundColor: darkMode ? '#000' : 'white',
+      height:50,
+      marginTop:25
+    },
+    ConfigText:{
+      color: darkMode ? '#d6d6d6' : 'black',
+      textAlign:'left',
+      fontSize:18,
+      marginTop:6,
+      marginLeft:25,
+      fontWeight:'semi bold'
+    },
+    ConfigTextRed:{
+      color:'#DD2E2E',
+      textAlign:'left',
+      fontSize:18,
+      marginTop:30,
+      marginLeft:25,
+      fontWeight:'semi bold'
+    }
+  })
+
+
   const onPressVoltarPerfil = () =>{
     navigation.navigate('Routes')
   }
   const navigation = useNavigation();
   
   const [isChecked, setChecked] = useState(false); /*Variável que deixa o checkbox da opção de desativar as notificações em estado false, ou seja, deixa o checkbox como desmarcado*/
-  const [isCheckedTwo, setCheckedTwo] = useState(false) /*Variável que deixa o checkbox da opção de colocar o app em modo escuro em estado false*/
   
   return (
    <View style={Css.container}>
@@ -35,75 +101,27 @@ export default function Configuracoes (){
       </View>
 
       <View style={styles.ViewConfig}>
-        <Text style={styles.textCheck}>Modo Escuro</Text>
-        <Checkbox style={styles.Checkbox} value={isCheckedTwo} onValueChange={setCheckedTwo}/>
+        <Text style={styles.textCheck}>Alterar Modo</Text>
+        
+         <TouchableOpacity style={styles.Mode}>
+         <MaterialCommunityIcons name="theme-light-dark" size={38} color={darkMode ? "#C6C6C6" : "#25252A"} 
+         onPress={()=> darkMode ? setDarkMode(false) : setDarkMode(true)}
+         />
+         </TouchableOpacity>
+
       </View>
 
       <View style={styles.ViewConfig}>
-       <TouchableOpacity style={styles.btnConfig}>
-        <Text style={styles.ConfigText}>Fazer Logout</Text>
+       <TouchableOpacity >
+        <Text style={styles.textCheck}>Fazer Logout</Text>
        </TouchableOpacity>
       </View>
 
       <View style={styles.ViewConfig}>
-       <TouchableOpacity style={styles.btnConfig}>
+       <TouchableOpacity>
         <Text style={styles.ConfigTextRed}>Desativar Conta</Text>
        </TouchableOpacity>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({ 
-  btnIconVoltar:{
-    alignSelf: 'flex-start',
-    top: -10, 
-  },
-  icon:{
-    width: 27,
-    height: 27,
-  },
-  ViewConfig:{
-    flex:0.12,
-    backgroundColor:'#000',
-    borderWidth:0.5,
-    borderBottomColor:'#d6d6d6',
-    marginTop:3
-  },
-  textCheck:{
-  color:'#d6d6d6',
-  textAlign:'left',
-  fontSize:18,
-  marginTop:30,
-  marginLeft:25,
-  fontWeight:'semi bold'
-  },
-  Checkbox:{
-    marginLeft:310,
-    marginTop:-30,
-    backgroundColor:'#000',
-    height:40,
-    width:40,
-    
-  },
-  btnConfig:{
-    backgroundColor:'black',
-    height:50,
-    marginTop:25
-  },
-  ConfigText:{
-    color:'#d6d6d6',
-    textAlign:'left',
-    fontSize:18,
-    marginTop:6,
-    marginLeft:25,
-    fontWeight:'semi bold'
-  },
-  ConfigTextRed:{
-    color:'#DD2E2E',
-    textAlign:'left',
-    fontSize:18,
-    marginTop:6,
-    marginLeft:25,
-  }
-})
