2d7f6a3987f9ac85db11d87e50ef6f3b786e85ec
Guilherme Mecenas <guilhermemecenasduarte@gmail.com>
2022-10-28 12:28:59 -0300

    Testando API

--- pages/Fluxo de autenticação/Cadastro.js
+++ pages/Fluxo de autenticação/Cadastro.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+import React, { useDebugValue, useState } from 'react';
+import { StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
 import CustomInput from '../../Componentes/CustomInput.js';
 import Css from '../css';
 import Vector from '../../img/Vector.png'
@@ -7,10 +7,27 @@ import CustomButton from '../../Componentes/CustomButton';
 import background_login_signup from '../../img/background_login_signup.png'
 import { useNavigation } from '@react-navigation/native';
 import {useForm} from 'react-hook-form'
+import {Teste} from '../../function.js';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 export default function Cadastro(){
 
+    const [teste, setTeste] = useState('')
+    
+    const Armazenar = (chave,valor) => {
+        AsyncStorage.setItem(chave,valor)
+    }
+    
+    const Buscar = async (chave) => {
+        const valor = await AsyncStorage.getItem(chave)
+        setTeste(valor)
+    }
+    
+    Armazenar('01', 'teste')
+    Armazenar('02', 'oi isso aqui é o id numero 2')
+    // Buscar('02')
+    
     const {control, handleSubmit, formState: {errors}} = useForm();
 
     console.log(errors);
@@ -20,11 +37,41 @@ export default function Cadastro(){
         
         navigation.navigate('Login')
     }
-    const onPressSigngUp = data => {
+    const onPressSigngUp = async data => {
 
-        console.log(data)
-        
-        navigation.navigate('Preferencias')
+        // Teste({
+        //     nome: data.Nome,
+        //     usuario: data.Usuario, 
+        //     email: data.Email,
+        //     senha: data.Senha, 
+        // })
+
+         try{            
+            await fetch('https://backend-sestante.herokuapp.com/user', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        name: data.Nome, 
+                        username: data.Usuario, 
+                        email: data.Email,
+                        password: data.Senha, 
+                    })
+                })
+                .then(response => response.json())
+                .then((responseJson) => {
+                    const resposta = (JSON.stringify(responseJson))
+                    console.log(resposta)
+                    // Armazenar('01', resposta)
+                })
+                
+                // .then(navigation.navigate('Preferencias'))
+            }
+            catch(error){
+                console.log(error)
+            }
+
     }
     
     return(
@@ -73,7 +120,8 @@ export default function Cadastro(){
                 onPress={onPressLogin}
                 type="TERTIARY"
                 />
-                
+                {/* {Buscar('01')} */}
+                <Text>{teste}</Text>
             </View>
             </ImageBackground>
         </View>
