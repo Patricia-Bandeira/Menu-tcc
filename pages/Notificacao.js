1a42095c93f74c4e973ea659cb23719ff9fa96a3
rkky33 <boltmat2015@gmail.com>
2022-11-26 15:40:30 -0300

    integração notificacao

--- pages/Notificacao.js
+++ pages/Notificacao.js
@@ -3,32 +3,87 @@ import Css from './css'
 import Vector from '../img/Vector.png'
 import nadaPorAqui from '../img/nadpAquiNotifi.png'
 import Container_notificacao from '../Componentes/componente.notificacao';
+import AS_API from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react';
 
 export default function Notificacao (){
-  let notificacoes = 0;
+
+  const [responsePending, setResponsePending] = useState(false)
+
+  const [notification, setNotification] = useState()
+
+  const getNotification = async () => {
+
+    const receivedToken = await AS_API.getItem('token')
+    const token = receivedToken.slice(1,-1)
+    const bearer = `Bearer ${token}`
+    
+
+    setResponsePending(true)
+
+    try {
+      fetch ('https://backend-sestante.herokuapp.com/user/notification',{
+        method: 'DELETE',
+        withCredentials: true,
+        credentials: 'include',
+            headers: {
+                'Authorization': bearer,
+                'Content-Type': 'application/json'},
+      })
+      .then(response => response.json())
+      .then(async responseJson => {
+        console.log(responseJson)
+        setNotification(responseJson)
+      }
+          )
+      
+    }
+
+    catch (error) {
+      console.log(error)
+    }
+    setResponsePending(false)
+  }
+
+  useEffect(() => {
+    getNotification()
+  },[] )
+
   return (
    <View style={Css.container}>
    
     <View style={Css.cabecalho}>
     <Image source={Vector} style={Css.img}/>
-       
-
           </View>
-          {notificacoes > 0 ?
-          <View>
-          <ScrollView>
-            <Container_notificacao/>
-            <Container_notificacao/>
-            <Container_notificacao/>
-          </ScrollView>
-          </View>
-            :
+          {notification.message === "E_ROUTE_NOT_FOUND: Cannot DELETE:/user/notification" ?
             <View style={Css.nadaPorAquipngView}>
             <Image
              source={nadaPorAqui} 
              style={Css.nadaPorAquipng}
              />
             </View>
+            :
+          <View>
+          {notification.map(notification => {
+            return(
+          <ScrollView>
+          <View key={notification.id} style={Css.card} >
+            <View style={Css.miniPerfilView}>
+                <Image
+                style={Css.miniPerfil}
+                source={notification.avatar === null ? userIcon : notification.avatar.url}
+                />
+            </View>
+            <Text style ={Css.nomeDeUsuario}>@{notification.user_reference.username}</Text>
+            <Text style ={Css.corponotifiacao}>{notification.description}</Text>
+            <Text>{notification.date}</Text>
+            <Text>{notification.user_reference.name}</Text>
+            <Text>{notification.user_reference.email}</Text>
+        </View>
+          </ScrollView>
+            )
+          })}
+          </View>
           }
          </View>
 
